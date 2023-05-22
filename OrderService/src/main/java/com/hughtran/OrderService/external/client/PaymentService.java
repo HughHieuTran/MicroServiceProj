45935f7f8d3e9101455bf07a10a0c3a78d25b8c9
John O'connor <thien.hieu2299@gmail.com>
2023-05-22 22:13:39 +0700

    .

--- OrderService/src/main/java/com/hughtran/OrderService/external/client/PaymentService.java
+++ OrderService/src/main/java/com/hughtran/OrderService/external/client/PaymentService.java
@@ -1,14 +1,21 @@
 package com.hughtran.OrderService.external.client;
 
+import com.hughtran.OrderService.exception.CustomException;
 import com.hughtran.OrderService.request.PaymentRequest;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@CircuitBreaker(name = "external",fallbackMethod = "fallback")
 @FeignClient(name = "PAYMENT-SERVICE/payment")
 public interface PaymentService {
     @PostMapping
     public ResponseEntity<Long> doPayment(@RequestBody PaymentRequest paymentRequest);
+
+    default void fallback(Exception e){
+        throw new CustomException("payment service is not active","UNAVAILABLE",500);
+    }
 
 }
--- OrderService/src/main/java/com/hughtran/OrderService/external/client/ProductService.java
+++ OrderService/src/main/java/com/hughtran/OrderService/external/client/ProductService.java
@@ -1,5 +1,7 @@
 package com.hughtran.OrderService.external.client;
 
+import com.hughtran.OrderService.exception.CustomException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,10 +9,13 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "PRODUCT-SERVICE/product")
+@CircuitBreaker(name = "external",fallbackMethod = "fallback")
 public interface ProductService {
     @PutMapping("/reduceQuantity/{id}")
     public ResponseEntity<Void> reduceQuantity(
             @PathVariable("id") long productId, @RequestParam long quantity
     );
-
+    default void fallback(Exception e){
+        throw new CustomException("product service is not active","UNAVAILABLE",500);
+    }
 }
