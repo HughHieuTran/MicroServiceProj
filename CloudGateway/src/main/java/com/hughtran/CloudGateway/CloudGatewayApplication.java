f075dface124a9c8bea82fdee7a3a5a04d43c3d4
unknown <thien.hieu2299@gmail.com>
2023-05-23 11:37:38 +0700

    add security

--- CloudGateway/src/main/java/com/hughtran/CloudGateway/CloudGatewayApplication.java
+++ CloudGateway/src/main/java/com/hughtran/CloudGateway/CloudGatewayApplication.java
@@ -7,6 +7,9 @@ import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuit
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class CloudGatewayApplication {
@@ -15,6 +18,10 @@ public class CloudGatewayApplication {
         SpringApplication.run(CloudGatewayApplication.class, args);
     }
 
+    @Bean
+    KeyResolver userKeyResolver(){
+        return exchange -> Mono.just("userKey");
+    }
     public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
         return factory -> factory.configureDefault(
                 id -> new Resilience4JConfigBuilder(id)
