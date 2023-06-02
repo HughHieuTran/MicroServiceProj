a70ceb72473ceb722832019af5f02d46e8c299b4
John O'connor <thien.hieu2299@gmail.com>
2023-06-02 21:21:13 +0700

    add k8s

--- CloudGateway/src/main/java/com/hughtran/CloudGateway/CloudGatewayApplication.java
+++ CloudGateway/src/main/java/com/hughtran/CloudGateway/CloudGatewayApplication.java
@@ -3,7 +3,6 @@ package com.hughtran.CloudGateway;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreaker;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
--- OrderService/src/main/java/com/hughtran/OrderService/controller/OrderController.java
+++ OrderService/src/main/java/com/hughtran/OrderService/controller/OrderController.java
@@ -1,10 +1,8 @@
 package com.hughtran.OrderService.controller;
 
-import com.hughtran.OrderService.entity.Order;
 import com.hughtran.OrderService.model.OrderRequest;
 import com.hughtran.OrderService.model.OrderResponse;
 import com.hughtran.OrderService.service.OrderService;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/order")
-@Log4j2
 public class OrderController {
 
     @Autowired
